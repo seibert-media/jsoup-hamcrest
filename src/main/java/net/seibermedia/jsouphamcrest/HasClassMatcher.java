6a992cfa2cacc23207e616a04a1dee0bc8f8244c
Peter Körner <pkoerner@seibert-media.net>
2019-07-02 16:50:35 +0200

    HasClassMatcher

---
+++ src/main/java/net/seibermedia/jsouphamcrest/HasClassMatcher.java
@@ -0,0 +1,79 @@
+package net.seibermedia.jsouphamcrest;
+
+import java.util.Set;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.Matchers;
+import org.hamcrest.TypeSafeDiagnosingMatcher;
+import org.jsoup.nodes.Element;
+
+public class HasClassMatcher extends TypeSafeDiagnosingMatcher<Element> {
+
+	private final Matcher<String> classMatcher;
+	private final Matcher<Iterable<? extends String>> classesMatcher;
+
+	private HasClassMatcher(Matcher<String> classMatcher, Matcher<Iterable<? extends String>> classesMatcher) {
+		this.classMatcher = classMatcher;
+		this.classesMatcher = classesMatcher;
+	}
+
+
+	public static HasClassMatcher hasClass(String cssClass) {
+		return new HasClassMatcher(Matchers.equalTo(cssClass), null);
+	}
+
+	public static HasClassMatcher hasClass(Matcher<String> cssClassMatcher) {
+		return new HasClassMatcher(cssClassMatcher, null);
+	}
+
+	public static HasClassMatcher hasClasses(String... cssClasses) {
+		return new HasClassMatcher(null, Matchers.containsInAnyOrder(cssClasses));
+	}
+
+	public static HasClassMatcher hasClasses(Matcher<Iterable<? extends String>> cssClassesMatcher) {
+		return new HasClassMatcher(null, cssClassesMatcher);
+	}
+
+	@Override
+	protected boolean matchesSafely(Element item, Description mismatchDescription) {
+		Set<String> classNames = item.classNames();
+
+		if (classMatcher != null) {
+			// check all classes individually. at lease one class must comply to the classMatcher
+
+			for (String className : classNames) {
+				if (classMatcher.matches(className)) {
+					return true;
+				}
+			}
+			mismatchDescription
+					.appendText("had no class ")
+					.appendDescriptionOf(classMatcher);
+
+			return false;
+		} else if (classesMatcher != null) {
+			if (!classesMatcher.matches(classNames)) {
+				mismatchDescription.appendText("has classes that ");
+				classesMatcher.describeMismatch(classNames, mismatchDescription);
+
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	@Override
+	public void describeTo(Description description) {
+		if (classMatcher != null) {
+			description
+					.appendText("has a class ")
+					.appendDescriptionOf(classMatcher);
+		} else if (classesMatcher != null) {
+			description
+					.appendText("has classes ")
+					.appendDescriptionOf(classesMatcher);
+		}
+	}
+}
---
+++ src/test/java/net/seibermedia/jsouphamcrest/HasClassMatcherTest.java
@@ -0,0 +1,153 @@
+package net.seibermedia.jsouphamcrest;
+
+import static net.seibermedia.jsouphamcrest.HasClassMatcher.hasClass;
+import static net.seibermedia.jsouphamcrest.HasClassMatcher.hasClasses;
+import static net.seibermedia.jsouphamcrest.HasElementMatcher.hasElement;
+import static net.seibermedia.jsouphamcrest.IsHtmlMatcher.isHtmlMatching;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.hamcrest.Matchers.endsWith;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+
+import org.hamcrest.StringDescription;
+import org.junit.Test;
+
+public class HasClassMatcherTest {
+	@Test
+	public void matchesClass() {
+		String html = "<div><br class='hello world'></div>";
+		assertThat(html, isHtmlMatching(hasElement("br", hasClass("hello"))));
+	}
+
+	@Test(expected = AssertionError.class)
+	public void rejectsClass() {
+		String html = "<div><br class='hello world'></div>";
+		assertThat(html, isHtmlMatching(hasElement("br", hasClass("bye"))));
+	}
+
+	@Test
+	public void matchesClassWithSubMatcher() {
+		String html = "<div><br class='fa far-world'></div>";
+		assertThat(html, isHtmlMatching(hasElement("br", hasClass(endsWith("-world")))));
+	}
+
+	@Test(expected = AssertionError.class)
+	public void rejectsClassWithSubMatcher() {
+		String html = "<div><br class='fa far-world'></div>";
+		assertThat(html, isHtmlMatching(hasElement("br", hasClass(endsWith("-icon")))));
+	}
+
+	@Test
+	public void matchesClasses() {
+		String html = "<div><br class='hello world'></div>";
+		assertThat(html, isHtmlMatching(hasElement("br", hasClasses("hello", "world"))));
+	}
+
+	@Test(expected = AssertionError.class)
+	public void rejectsClasses() {
+		String html = "<div><br class='hello world'></div>";
+		assertThat(html, isHtmlMatching(hasElement("br", hasClasses("bye", "world"))));
+	}
+
+	@Test
+	public void matchesClassesWithSubMatcher() {
+		String html = "<div><br class='fa far-world'></div>";
+
+		//noinspection unchecked
+		assertThat(html, isHtmlMatching(hasElement("br", hasClasses(containsInAnyOrder(
+				equalTo("fa"),
+				endsWith("-world")))
+		)));
+	}
+
+	@Test(expected = AssertionError.class)
+	public void rejectsClassesWithSubMatcher() {
+		String html = "<div><br class='fa far-world'></div>";
+
+		//noinspection unchecked
+		assertThat(html, isHtmlMatching(hasElement("br", hasClasses(containsInAnyOrder(
+				equalTo("fa"), endsWith("-icon")))
+		)));
+	}
+
+	@Test
+	public void describesHasClassCorrectly() {
+		String html = "<div><br class='hello world'></div>";
+		IsHtmlMatcher matcher = isHtmlMatching(hasElement("br", hasClass("bye")));
+
+		StringDescription expectedDescription = new StringDescription();
+		matcher.describeTo(expectedDescription);
+
+		StringDescription mismatchDescription = new StringDescription();
+		boolean matches = matcher.matchesSafely(html, mismatchDescription);
+
+		assertThat(matches, is(false));
+		assertThat(expectedDescription
+				.toString(), is("a parsable HTML-Document that has an element matching \"br\" that has a class \"bye\""));
+
+		assertThat(mismatchDescription.toString(), is("a parsable HTML-Document that did have an element matching \"br\" " +
+				"that had no class \"bye\""));
+	}
+
+	@Test
+	public void describesHasClassWithSubMatcherCorrectly() {
+		String html = "<div><br class='fa far-world'></div>";
+		IsHtmlMatcher matcher = isHtmlMatching(hasElement("br", hasClass(endsWith("-icon"))));
+
+		StringDescription expectedDescription = new StringDescription();
+		matcher.describeTo(expectedDescription);
+
+		StringDescription mismatchDescription = new StringDescription();
+		boolean matches = matcher.matchesSafely(html, mismatchDescription);
+
+		assertThat(matches, is(false));
+		assertThat(expectedDescription.toString(), is("a parsable HTML-Document that has an element matching \"br\" that " +
+				"has a class a string ending with \"-icon\""));
+
+		assertThat(mismatchDescription.toString(), is("a parsable HTML-Document that did have an element matching \"br\" that " +
+				"had no class a string ending with \"-icon\""));
+	}
+
+	@Test
+	public void describesHasClassesCorrectly() {
+		String html = "<div><br class='hello world'></div>";
+		IsHtmlMatcher matcher = isHtmlMatching(hasElement("br", hasClasses("bye", "world")));
+
+		StringDescription expectedDescription = new StringDescription();
+		matcher.describeTo(expectedDescription);
+
+		StringDescription mismatchDescription = new StringDescription();
+		boolean matches = matcher.matchesSafely(html, mismatchDescription);
+
+		assertThat(matches, is(false));
+		assertThat(expectedDescription.toString(), is("a parsable HTML-Document that has an element matching \"br\" " +
+				"that has classes iterable with items [\"bye\", \"world\"] in any order"));
+
+		assertThat(mismatchDescription.toString(), is("a parsable HTML-Document that did have an element matching \"br\" " +
+				"that has classes that not matched: \"hello\""));
+	}
+
+	@Test
+	public void describesHasClassesWithSubMatcherCorrectly() {
+		String html = "<div><br class='fa far-world'></div>";
+
+		//noinspection unchecked
+		IsHtmlMatcher matcher = isHtmlMatching(hasElement("br", hasClasses(containsInAnyOrder(
+				equalTo("fa"), endsWith("-icon")))
+		));
+
+		StringDescription expectedDescription = new StringDescription();
+		matcher.describeTo(expectedDescription);
+
+		StringDescription mismatchDescription = new StringDescription();
+		boolean matches = matcher.matchesSafely(html, mismatchDescription);
+
+		assertThat(matches, is(false));
+		assertThat(expectedDescription.toString(), is("a parsable HTML-Document that has an element matching \"br\" " +
+				"that has classes iterable with items [\"fa\", a string ending with \"-icon\"] in any order"));
+
+		assertThat(mismatchDescription.toString(), is("a parsable HTML-Document that did have an element matching \"br\" " +
+				"that has classes that not matched: \"far-world\""));
+	}
+}
